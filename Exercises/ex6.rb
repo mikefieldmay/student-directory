f37980b6bb40d8c1b4d13ce6f0d883b5d92cd2db
Mike Field-May <mfieldmay@gmail.com>
2016-12-06 10:43:38 +0000

    Finished Exercise 6

--- Exercises/ex6.rb
+++ Exercises/ex6.rb
@@ -1,7 +1,7 @@
 # lets put all the students into an array
 def input_students
-  puts "Please enter the names of the students.".center(20)
-  puts "To finish, just hit return twice.".center(20)
+  puts "Please enter the names of the students.".center(50)
+  puts "To finish, just hit return twice.".center(50)
   # create an empty array
   students = []
   # get the first name
@@ -9,12 +9,12 @@ def input_students
   # while the name is not empty repeat this code
   while !name.empty? do
     # add the student to the hash array
-    puts "What cohort are they in?"
+    puts "What cohort are they in?".center(50)
     cohort = gets.chomp
     cohort.to_s == '' ? :unknown : cohort
     students << {name: name, cohort: (cohort.to_s == '' ? :unknown : cohort)}
 
-    puts "Now we have #{students.count} students"
+    puts "Now we have #{students.count} students".center(50)
     # get another name from the user
     name = gets.chomp
   end
@@ -24,18 +24,18 @@ end
 
 # and then we print them
 def print_header
-  puts "The students of Villains Academy"
-  puts "----------"
+  puts "The students of Villains Academy".center(50)
+  puts "----------".center(50)
 end
 
 def print(students)
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{student[:name]} (#{student[:cohort]} cohort)".center(50)
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students".center(50)
 end
 
 students = input_students
