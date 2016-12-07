1d4e9a6505d2ac52cb855e3ced0cf1509333f668
Mike Field-May <mfieldmay@gmail.com>
2016-12-07 13:24:00 +0000

    Annotated ex8part2

---
+++ ExercisesChap14/ex8pt2rb.rb
@@ -0,0 +1,6 @@
+_="_=%p;puts _%%_";puts _%_
+
+# quine I found online which prints its own code.
+# puts replaces %p with _ which inserts the value of _ into the string.
+# It seems that two %% together will cancel the replacement of a character
+# Maybe like \n but not completely sure. Couldn't find documentation
